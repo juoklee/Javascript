c4f3f62177fa8c0d8a743ef75ec5c403920f55a3
이주옥 <juok9802@gmail.com>
2022-02-07 23:39:52 +0900

    update

--- js/sale.js
+++ js/sale.js
@@ -8,6 +8,6 @@ function showPrice() {
     var resultPrice = oPrice - savedPrice;
   }
 
-  document.querySelector("#showResult").innerHTML = "상품의 원래 가격은 " + oPrice +"원이고, 할인율은 " + rate + "%입니다." + savedPrice + " 원을 절약한 " + resultPrice + "원에 살 수 있습니다.";
+  document.querySelector("#showResult").innerHTML = "상품의 원래 가격은 " + oPrice +"원이고, 할인율은 " + rate + "%입니다. " + savedPrice + " 원을 절약한 " + resultPrice + "원에 살 수 있습니다.";
 
 }
